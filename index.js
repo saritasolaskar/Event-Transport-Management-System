a03ba661a98ccbf90277d2ad3ac9cbd7e286c828
saritasolaskar <saritasolaskar@gmail.com>
2026-03-17 15:59:32 +0530

    .

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const mongoose = require("mongoose");
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect('mongodb+srv://saritasolaskar:Sarita%402005@cluster0.4pe3ccg.mongodb.net/?appName=Cluster0');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
