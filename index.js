33a63709b720b7ab51209744326de4c23ccf757b
saritasolaskar <saritasolaskar@gmail.com>
2026-03-17 16:43:31 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
- 
+  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -31,7 +31,7 @@ async function main() {
 
 
 //common paths
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${port}`);
